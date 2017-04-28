d449b7f440fbb3f6842052316d490e1669e6d8d9
Kim Hanseulmaro <hanseulmaro.kim@ciceron.me>
2017-04-29 02:03:39 +0900

    The smell of Sungmin's feet is terrible.

--- app.js
+++ app.js
@@ -6,9 +6,20 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
+
+// var session = require('express-session');
+
+try{
+  var nodeadmin = require('nodeadmin');
+  app.use(nodeadmin(app));
+
+}
+catch(e){
+
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,13 +28,13 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
